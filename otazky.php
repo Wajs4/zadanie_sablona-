59b3ae6a16fa20686c96db27bc2857e456824b2d
Wajs4 <tobias.biely@student.ukf.sk>
2026-03-12 23:07:03 +0100

    Add files via upload

---
+++ otazky.php
@@ -0,0 +1,13 @@
+<?php
+$otazky = array(
+    "Aké sú vaše skúsenosti s PHP?"
+    "Aké je najlepšie Jedlo?"
+    "Čo si dnes ládoval?"
+);
+
+$odpovede = array(
+    "Mám základné skúsenosti s PHP"
+    "Jednoznacne pizza .."
+    "Dal som si kilo himalajskej soli"
+);
+?>
